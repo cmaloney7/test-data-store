247134160c34cb805eba6a3f3222d0b2c2e1ced0
Corey Molloy <cmolloy@hollandamericagroup.com>
2019-08-19 22:12:11 -0700

    swagger - implemented swagger, but still need to finish writting jsdocs for all routes

--- config/db.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    // To connect using a driver via the standard MongoDB URI
-    // Example: mongodb://<dbuser>:<dbpassword>@ds121343.mlab.com:21343/<dbname>
-    // url : "mongodb://localhost:32772/testdatastore"
-    // url : "mongodb://localhost:27017/config"
-    // url : "mongodb://root:example@mongo:27017/testdatastore"
-    url : "mongodb://mongo:27017/testdatastore"
-};
--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
-const db             = require('./config/db');
+const config         = require('./config/app');
+const swaggerUi      = require('swagger-ui-express');
+const swaggerJsdoc   = require('swagger-jsdoc');
+const swaggerDefinition = require('./swagger/definition.js');
 
 const app = express();
-const hostname = '127.0.0.1';
-const port = 12700;
+const hostname = config.hostname;
+const port = config.port;
+const swaggerSpec = swaggerJsdoc(swaggerDefinition.options);
 
 app.use(bodyParser.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-MongoClient.connect(db.url, (err, database) => {
+MongoClient.connect(config.mongoUrl, (err, database) => {
     if (err) return console.log(err);
 
     // Make sure you add the mongo database name and not the collection name
-    var db = database.db("testdatastore");
+    var db = database.db(config.mongoDb);
     require('./app/routes')(app, db);
 
     app.listen(port, () => {
