db6afe4690a6cf696f85f2eff1b82c53ae95417e
Corey Molloy <cmolloy@hollandamericagroup.com>
2019-08-19 23:24:57 -0700

    tweaks

--- config/app.js
+++ config/app.js
@@ -3,8 +3,8 @@ module.exports = {
     // Example: mongodb://<dbuser>:<dbpassword>@ds121343.mlab.com:21343/<dbname>
     // mongoUrl : 'mongodb://localhost:27017/config',
     // mongoUrl : 'mongodb://root:example@mongo:27017/testdatastore',
-    // mongoUrl : 'mongodb://mongo:27017/testdatastore',    // use for regular use
-    mongoUrl : 'mongodb://localhost:32780/testdatastore',   // local dev only using docker, Mongo container
+    // mongoUrl : 'mongodb://localhost:32780/testdatastore',   // local dev only using docker, Mongo container
+    mongoUrl : 'mongodb://mongo:27017/testdatastore',    // use for regular use
     mongoDb: 'testdatastore',
     hostname: '127.0.0.1',
     port: 12700
