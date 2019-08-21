b3682b847c1a370234a0fe27f860fd83ed00491c
Corey Molloy <cmolloy@hollandamericagroup.com>
2019-08-20 22:19:05 -0700

    tag, creation and storage of api image in docker hub repo

---
+++ docker.push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# login to docker hub
+echo logging into to docker hub...
+docker login --username caselletuckelle
+#docker login --username=$DOCKER_USER --password=$DOCKER_PASS $DOCKER_HOST
+#docker login --username caselletuckelle --password-stdin
+
+# tag container
+docker tag test-data-store:latest caselletuckelle/test-data-store:latest
+
+# build new app container
+docker push caselletuckelle/test-data-store:latest
