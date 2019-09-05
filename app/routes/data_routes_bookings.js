badac65f8f74a8c832f8c2f6ecb45e991e81b798
Corey Molloy <cmolloy@hollandamericagroup.com>
2019-09-04 17:09:14 -0700

    finish swagger page

--- app/routes/data_routes_bookings.js
+++ app/routes/data_routes_bookings.js
@@ -1,10 +1,10 @@
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
-  var mongoCollection = "bookings";
+  var mongoCollection = "bookings';
 
   // GET
-  app.get("/bookings", (req, res) => {
+  app.get('/bookings', (req, res) => {
     const data = {
       polarRegion: req.query.polarRegion,
       brand: req.query.brand,
@@ -13,7 +13,7 @@ module.exports = function(app, db) {
     };
 
     // set reservedBy to null if not passed in JSON request. This will prevent reserved data from being rqeturned.
-    if (data.reservedBy === "undefined") data.reservedBy = null;
+    if (data.reservedBy === 'undefined') data.reservedBy = null;
 
     // set mongo query
     var query = {
@@ -28,19 +28,19 @@ module.exports = function(app, db) {
       .find(query)
       .toArray(function(err, result) {
         if (err) {
-          console.log("Mongo Query:");
+          console.log('Mongo Query:');
           console.log(query);
-          res.send({ error: "An error has occurred" });
-          console.log("GET ERROR: " + err);
+          res.send({ error: 'An error has occurred' });
+          console.log('GET ERROR: ' + err);
         } else {
           res.send(result);
-          console.log("GET Booking Collection(s): " + result.length);
+          console.log('GET Booking Collection(s): ' + result.length);
         }
       });
   });
 
   // POST
-  app.post("/booking", (req, res) => {
+  app.post('/booking', (req, res) => {
     const data = {
       reservedBy: req.body.reservedBy,
       bookingNumber: req.body.bookingNumber,
@@ -62,17 +62,17 @@ module.exports = function(app, db) {
     // insert record object in mongo collection
     db.collection(mongoCollection).insertOne(data, (err, result) => {
       if (err) {
-        res.send({ error: "An error has occurred" });
-        console.log("POST ERROR: " + err);
+        res.send({ error: 'An error has occurred' });
+        console.log('POST ERROR: ' + err);
       } else {
         res.send(result.ops[0]);
-        console.log("POST Data: " + result.ops[0]._id);
+        console.log('POST Data: ' + result.ops[0]._id);
       }
     });
   });
 
   //PUT - booking
-  app.put("/booking/:id", (req, res) => {
+  app.put('/booking/:id', (req, res) => {
     const id = req.params.id;
     const details = { _id: new ObjectID(id) };
     const data = {
@@ -93,27 +93,27 @@ module.exports = function(app, db) {
     data.modifiedDate = new Date().toISOString();
     db.collection(mongoCollection).updateOne(details, data, (err, result) => {
       if (err) {
-        res.send({ error: "An error has occurred" });
-        console.log("PUT ERROR: " + err);
+        res.send({ error: 'An error has occurred' });
+        console.log('PUT ERROR: ' + err);
       } else {
         res.send(data);
-        console.log("PUT Booking: " + id);
+        console.log('PUT Booking: ' + id);
       }
     });
   });
 
   // DELETE booking
-  app.delete("/booking/:id", (req, res) => {
+  app.delete('/booking/:id', (req, res) => {
     const id = req.params.id;
     const details = { _id: new ObjectID(id) };
 
     db.collection(mongoCollection).deleteOne(details, (err, item) => {
       if (err) {
-        res.send({ error: "An error has occurred" });
-        console.log("DELETE ERROR: " + err);
+        res.send({ error: 'An error has occurred' });
+        console.log('DELETE ERROR: ' + err);
       } else {
-        res.send("DELETED Data: " + id);
-        console.log("DELETED Data: " + id);
+        res.send('DELETED Data: ' + id);
+        console.log('DELETED Data: ' + id);
       }
     });
   });
